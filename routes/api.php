94f3f762faffddfb6af632e739f721a850472d33
danielwahrn <56650395+gitdevstar@users.noreply.github.com>
2021-05-26 21:15:23 +0800

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Resource\CardResource;
+use Checkout\CheckoutApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/card',    [CardResource::class]);
+Route::post('/payment',     'CheckoutApi@payment');
